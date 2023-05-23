e8ab160bcc0d9bb6fb694ab3bdbb32670d541188
thomasbogue <thomasbogue@gmail.com>
2023-05-23 16:01:12 -0400

    fixed issue with turn time being ignored

--- Assets/Entities/Entity.cs
+++ Assets/Entities/Entity.cs
@@ -18,7 +18,7 @@ public class Entity : MonoBehaviour
     public float maxAcceleration { get {return maxSpeed / responseTime;} }
     [Tooltip("the maximum acceleration to the side (i.e. to turn)")]
     public float responseTime { get {return entityType.responseTime;} }
-    public float maxTurningAcceleration { get {return Mathf.PI * maxSpeed / responseTime ;}}
+    public float maxTurningAcceleration { get {return Mathf.PI * maxSpeed / entityType.turnTime;}}
     public float maxSpeed { get {
         float speed = entityType.speed;
         foreach(Strength strength in strengths) {
